5ecc4a1c73adbad2d2c504fb5edecc2a4f8e7063
Sara <you@example.com>
2020-10-19 16:01:02 +0200

    finished 1 & 2

---
+++ Sara/index.js
@@ -0,0 +1,10 @@
+// 1. Write program to picking random country from countries.
+let randomCountry = ['Germany', 'Italy', 'USA'] ;
+console.log('randomCountry ==>', randomCountry[Math.floor(Math.random() * randomCountry.length)]) ;
+
+// 2. Write program to print random name from names in the following format 'Hallo ' and the random name.
+let randomName = ['Alex', 'Max', 'Tommy'] ;
+console.log('randomName ==>', 'Hello ' + randomName[Math.floor(Math.random() * randomName.length)]) ; // Hello Alex
+
+// 3. Write program to picking number name from numbers. The output Should be absolute (not negative numbers).
+
