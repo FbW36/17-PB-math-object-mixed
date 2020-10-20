6232ebe74b03c49efede3e53bc7075d36f247454
Muhasin <mahmudmuhasin@gmail.com>
2020-10-20 12:08:27 +0200

    rewrite the code

--- muhasin/index.js
+++ muhasin/index.js
@@ -1,17 +1,19 @@
 // 1. picking random country from countries
 let countries = "Germany Spain Italy USA";
-let random = countries.split(/\s+/)
+let random = countries.split(/\s+/);
 
 let randomCountry = random[Math.floor(Math.random() * random.length)]; // select random word
 console.log("randomCountry ==>", randomCountry);
 
-// 2. print random name 
-let names = "Alex Max Tommy"
-let name = names.split(/\s+/)
+// 2. print random name
+let names = "Alex Max Tommy";
+let name = names.split(/\s+/);
 
 let randomName = name[Math.floor(Math.random() * name.length)]; // select random word
 console.log("randomName ==>", randomName);
 
-/* let numbers = "-1 7 -33 -8 9"
-let notNegative = ;
-console.log(notNegative) */
+// 3
+let numbers = "-1 7 -33 -8 9";
+let sep = numbers.split(" ");
+let pickNumber = sep[Math.floor(Math.random() * 5)];
+console.log("picking a number ==>", Math.abs(pickNumber));
