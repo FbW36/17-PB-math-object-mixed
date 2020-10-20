cb2a9f9c97f66810a1ade0cf39cd84507d2415d8
Elias Guderian <46761652+GuderianE@users.noreply.github.com>
2020-10-20 10:09:07 +0200

    finsihed task for real

--- Elias/exercise-17.js
+++ Elias/exercise-17.js
@@ -6,3 +6,23 @@ const countryPicker = () => {
 }
 
 countryPicker();
+
+const namePicker = () => {
+    let names = "Alex Max Tommy";
+    names = names.split(" ");
+    namesArr = names[Math.floor(Math.random() * names.length)];
+    console.log("Hello ", namesArr);
+}
+
+namePicker();
+
+const numberPicker = () => {
+    let numbers = "-1 7 -33 -8 9";
+    numbers = numbers.split(" ");
+    numbersArr = numbers[Math.floor(Math.random() * numbers.length)];
+    absNumber = Math.abs(numbersArr);
+    console.log(absNumber);
+}
+
+numberPicker();
+
