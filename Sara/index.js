c478ad66f8175e7cbb86a20c9433c91e91ecac8f
Sara <you@example.com>
2020-10-20 14:40:17 +0200

    added changes

--- Sara/index.js
+++ Sara/index.js
@@ -1,10 +1,21 @@
 // 1. Write program to picking random country from countries.
-let randomCountry = ['Germany', 'Italy', 'USA'] ;
-console.log('randomCountry ==>', randomCountry[Math.floor(Math.random() * randomCountry.length)]) ;
+let country = 'Germany Spain Italy USA' ;
+let splitCountry = country.split(' ') ;
+let randomCountry = splitCountry[Math.floor(Math.random() * 4)] ; // 
+console.log('randomCountry ==>', randomCountry) ; // 
 
 // 2. Write program to print random name from names in the following format 'Hallo ' and the random name.
-let randomName = ['Alex', 'Max', 'Tommy'] ;
-console.log('randomName ==>', 'Hello ' + randomName[Math.floor(Math.random() * randomName.length)]) ; // Hello Alex
+let names = 'Alex Max Tommy' ;
+let splitName = names.split(' ') ;
+let randomName = splitName[Math.floor(Math.random() * 3)] ; // 
+console.log('randomName ==>', randomName ); //
+console.log('message ==>', 'Hello', `${randomName}`) ; // 
 
 // 3. Write program to picking number name from numbers. The output Should be absolute (not negative numbers).
+let number = '-1 7 -33 -8 9' ;
+let splitNumber = number.split(' ') ;
+console.log('length of number ==>', splitNumber.length) ; // 5
+let randomNumber = splitNumber[Math.floor(Math.random() * 5)]; //
+console.log('randomNumber ==>', Math.abs(randomNumber)); // 
 
+  
